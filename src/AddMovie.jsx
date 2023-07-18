f3ab4cfb2f52906aa34cae49ec28d04fb862b0b6
Jeremiah Ang <jeremiahang94@gmail.com>
2023-07-18 11:57:26 +0000

    Change back to useState for form

--- src/AddMovie.jsx
+++ src/AddMovie.jsx
@@ -1,32 +1,25 @@
 import React from 'react';
 
 export default function AddMovie(props) {
-    const titleRef = React.useRef();
-    const genreRef = React.useRef();
-
-    React.useEffect(() => {
-        if (props.movie) {
-            titleRef.current.value = props.movie.title;
-            genreRef.current.value = props.movie.genre;
-        }
-    }, []);
+    const [title, setTitle] = React.useState(props.movie ? props.movie.title : '');
+    const [genre, setGenre] = React.useState(props.movie ? props.movie.genre : '');
 
     return (
         <div>
             <form
                 onSubmit={(e) => {
                     e.preventDefault();
-                    props.onSubmit({ title: titleRef.current.value, genre: genreRef.current.value });
+                    props.onSubmit({ title: title, genre: genre });
                     return false;
                 }}
             >
                 <label>
                     Title:
-                    <input ref={titleRef} type="text" name="title" />
+                    <input type="text" name="title" value={title} onChange={(e) => setTitle(e.target.value)} />
                 </label>
                 <label>
                     Genre:
-                    <select ref={genreRef} name="genre">
+                    <select name="genre" value={genre} onChange={(e) => setGenre(e.target.value)}>
                         <option>Action</option>
                         <option>Adventure</option>
                         <option>Horror</option>
